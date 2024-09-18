8be786dd506c0f73c6da2aa2548c9f1c6071051f
Nico786 <nicolas.theye@gmail.com>
2024-09-18 12:43:24 +0200

    prevent saving instance if no picture joined

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Arrangement, Furniture
-
+from django.core.exceptions import ValidationError
 
 admin.site.site_header = "jonquilleBois - Administration"
-    
+
 admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Arrangement)
-admin.site.register(Furniture)
+
+class RealisationAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.clean()
+        except ValidationError as e:
+            # Affiche le message d'erreur si la validation échoue
+            self.message_user(request, str(e), level=messages.ERROR)
+            return
+        super().save_model(request, obj, form, change)
+        
+@admin.register(Arrangement)
+class ArrangementAdmin(RealisationAdmin):
+    pass
+
+@admin.register(Furniture)
+class FurnitureAdmin(RealisationAdmin):
+    pass
+        
+
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.db.models import Model
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, TextField, ImageField
+from django.core.exceptions import ValidationError
 
 class CustomUser(AbstractUser):
   phone = CharField(max_length=15, unique=True, null=True, blank=True, default="")
@@ -19,6 +20,11 @@ class Realisation(Model):
 
   def __str__(self):
     return self.title
+  
+  def clean(self):
+        super().clean()
+        if not self.picture:
+            raise ValidationError('La photo est requise pour valider la création.')
 
 class Arrangement(Realisation):
   class Meta:
