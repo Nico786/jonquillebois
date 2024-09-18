ecc2eb7f0207320cffa225e77594922f5f7956fe
Nico786 <nicolas.theye@gmail.com>
2024-09-18 08:38:35 +0200

    add static files and upload files storage settings

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('mobilier', views.furnitures, name="furnitures"),
     path('contact', views.contact, name="contact"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import environ
 
@@ -114,7 +115,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+#Where are static files for dev:
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
+#After collectstatic - prod:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+# Uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'app.CustomUser'
 
