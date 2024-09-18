c3e774951ce8f1e0ab8d640f9ac6b42b29889942
Nico786 <nicolas.theye@gmail.com>
2024-09-18 09:35:20 +0200

    add logger

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,23 @@ AUTH_USER_MODEL = 'app.CustomUser'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',  
+        },
+        '__main__': {  
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
