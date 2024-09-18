35a3d1ef6ae0e9fa0430a3b84b89fa4ab6d1bd4a
Nico786 <nicolas.theye@gmail.com>
2024-09-18 10:26:12 +0200

    misc

--- app/admin.py
+++ app/admin.py
@@ -3,13 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Arrangement, Furniture
 
 
-class AgencementAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')  # Colonnes à afficher dans la liste
-    search_fields = ('title',)  # Barre de recherche sur le titre
-
-class MobilierAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
-    search_fields = ('title',)
+admin.site.site_header = "jonquilleBois - Administration"
     
 admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Arrangement)
