a57acf113eca841c830e2d46b696d89cf122805c
Nico786 <nicolas.theye@gmail.com>
2024-09-29 11:23:06 +0200

    remove SILENCED_SYSTEM_CHECKS param for prod ReCaptcha

--- project/settings.py
+++ project/settings.py
@@ -206,4 +206,4 @@ RECAPTCHA_PUBLIC_KEY = env.str('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = env.str('RECAPTCHA_PRIVATE_KEY')
 # RECAPTCHA_PROXY = {'http': 'http://127.0.0.1:8000', 'https': 'https://127.0.0.1:8000'}
 # RECAPTCHA_DOMAIN = 'www.recaptcha.net'
-SILENCED_SYSTEM_CHECKS = ['django_recaptcha.recaptcha_test_key_error'] #default keys for dev
+# SILENCED_SYSTEM_CHECKS = ['django_recaptcha.recaptcha_test_key_error'] #default keys for dev
